fa5803326a7fd85c60868925f6d436c77698aeca
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-26 00:04:48 -0400

    added logic to movieThis function that is called when user inputs movie-this command. function will take movie that user inputted and use this to run OMDB API call. specific data is then pulled from the response and displayed in the console.

--- liri.js
+++ liri.js
@@ -13,18 +13,23 @@ var command = process.argv[2];
 // Slice process.argv array starting at 3 index and join the resulting new array to form a string for the API query
 var songInput = process.argv.slice(3).join(" ");
 
+// Variable storing user input for OMDB and Bands In Town APIs
+// Slice process.argv array starting at 3 index and join the resulting new array with '+' symbol for queryUrl
+var axiosInput = process.argv.slice(3).join("+");
+
 // Function that will be called when user inputs spotify-this-song command
 function spotifyThis(input) {
-// Use search method from spotify node module to make call to spotify API
+// Use search method from spotify node module to make a call to spotify API
 spotify
   .search({ type: 'track', query: input, limit: 1 })
   .then(function(response) {
-        // console.log artist(s), song name, spotify link, album
+        // Grab specific data from the response, i.e. artist(s), song name, spotify link, and album
         let artist = response.tracks.items[0].artists[0].name;
         let song = response.tracks.items[0].name;
         let album = response.tracks.items[0].album.name;
         let link = response.tracks.items[0].external_urls.spotify;
         
+        // Display song data in console
         console.log(`Artist Name: ${artist}`);
         console.log(`Song: ${song}`);
         console.log(`Album: ${album}`);
@@ -39,8 +44,30 @@ function concertThis() {
 
 }
 
-function movieThis() {
+function movieThis(input) {
 
+    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        (response) => {
+            let title = response.data.Title;
+            let year = response.data.Year;
+            let imdb = response.data.imdbRating;
+            let rotten = response.data.Ratings[1].Value;
+            let country = response.data.Country;
+            let language = response.data.Language;
+            let plot = response.data.Plot;
+            let actors = response.data.Actors;
+            
+            console.log(`Title: ${title}`);
+            console.log(`Release Year: ${year}`);
+            console.log(`IMDB Rating: ${imdb}`);
+            console.log(`Rotten Tomatoes Rating: ${rotten}`);
+            console.log(`Produced in: ${country}`);
+            console.log(`Language: ${language}`);
+            console.log(`Plot: ${plot}`);
+            console.log(`Actors: ${actors}`); 
+        })
 }
 
 function doWhatItSays() {
@@ -53,7 +80,7 @@ switch(command) {
         break;
 
     case "movie-this":
-        movieThis();
+        movieThis(axiosInput);
         break;
 
     case "concert-this":
