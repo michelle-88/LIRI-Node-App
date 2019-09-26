c9fa78c9f1da6863547f89ec0b77d6d2b46f2e05
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-25 23:34:33 -0400

    added logic to spotifyThis function. the function will make a call to the spotify API, grab song data from the response, and display that data in the console.

--- liri.js
+++ liri.js
@@ -6,19 +6,29 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
-// Use this in switch
+// Store 3rd argument entered into CLI as variable which we will use in switch
 var command = process.argv[2];
-// Need to loop over process.argv array to capture all words in artist/movie/song name
-var userInput = process.argv[3];
 
-// Create 4 functions that will do something for each of the 4 commands and call them in switch
+// Variable storing user song input for spotify command
+// Slice process.argv array starting at 3 index and join the resulting new array to form a string for the API query
+var songInput = process.argv.slice(3).join(" ");
 
+// Function that will be called when user inputs spotify-this-song command
 function spotifyThis(input) {
-
+// Use search method from spotify node module to make call to spotify API
 spotify
   .search({ type: 'track', query: input, limit: 1 })
   .then(function(response) {
-        console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
+        // console.log artist(s), song name, spotify link, album
+        let artist = response.tracks.items[0].artists[0].name;
+        let song = response.tracks.items[0].name;
+        let album = response.tracks.items[0].album.name;
+        let link = response.tracks.items[0].external_urls.spotify;
+        
+        console.log(`Artist Name: ${artist}`);
+        console.log(`Song: ${song}`);
+        console.log(`Album: ${album}`);
+        console.log(`Link to Spotify: ${link}`);
   })
   .catch(function(err) {
         console.log(err);
@@ -39,7 +49,7 @@ function doWhatItSays() {
 
 switch(command) {
     case "spotify-this-song":
-        spotifyThis(userInput);
+        spotifyThis(songInput);
         break;
 
     case "movie-this":
